8ec9471d611ded6a8b1f284b3883c2d28d24cb22
Aulia Sabri <auliasabri43@gmail.com>
2020-11-04 16:55:31 +0700

    Updates

---
+++ precache-manifest.e8463b8da2734065fe94c81e571875d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c4189d37b458982d7e5310b8b50381",
+    "url": "/react-lab/index.html"
+  },
+  {
+    "revision": "b71794e2213212993d52",
+    "url": "/react-lab/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "092cbe7d3fae16a250b4",
+    "url": "/react-lab/static/css/main.7e7947e9.chunk.css"
+  },
+  {
+    "revision": "b71794e2213212993d52",
+    "url": "/react-lab/static/js/2.88c4d854.chunk.js"
+  },
+  {
+    "revision": "21fa4dc1016170aac15d36d5c5351696",
+    "url": "/react-lab/static/js/2.88c4d854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092cbe7d3fae16a250b4",
+    "url": "/react-lab/static/js/main.af60cbe5.chunk.js"
+  },
+  {
+    "revision": "c142acbd8f987c7de50e",
+    "url": "/react-lab/static/js/runtime-main.6c5157c1.js"
+  }
+]);
